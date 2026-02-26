ebb0d43fe34daf3db45b9696d844e34f9a2fb2ef
DeborahK <deborahk@insteptech.com>
2026-02-26 14:18:03 -0800

    Comment out code to start

--- first-signal-form-begin/src/app/user/user.service.ts
+++ first-signal-form-begin/src/app/user/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { UserProfile } from './user-profile';
 
 @Injectable({
   providedIn: 'root',
@@ -7,10 +6,10 @@ import { UserProfile } from './user-profile';
 export class UserService {
   // Fake API implementation for now
   // If working with RxJS, use firstValueFrom() or lastValueFrom()
-  async save(value: UserProfile) {
-    console.log('Saving data: ', JSON.stringify(value));
-    const resp = { status: 200, message: 'Success' };
-    await new Promise(r => setTimeout(r, 2000));
-    return resp;
-  }
+  // async save(value: UserProfile) {
+  //   console.log('Saving data: ', JSON.stringify(value));
+  //   const resp = { status: 200, message: 'Success' };
+  //   await new Promise(r => setTimeout(r, 2000));
+  //   return resp;
+  // }
 }
